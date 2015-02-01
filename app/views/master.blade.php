1c3e47015795ee9af8753808ecff2dbbda989320
Tanprasit <tanprasitp@hotmail.co.uk>
2015-02-01 12:56:45 +0000

    First iteration

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -2,8 +2,174 @@
 <head>
 	<title>Portfolio - Tanprasit.co.uk</title>
 	<meta charset="UTF-8">
+	    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/grayscale.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-hello world
+	<!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-play-circle"></i>  <span class="light">TANPRASIT.CO.UK</span>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past intro section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#">ABOUT ME</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">PORTFOLIO</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#resume">RESUME</a>
+                    </li>
+                    <li>
+                    	<a class="page-scroll" href="#contact">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body" id="page-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                    	<img class="img-circle center-block" src="img/Me.jpg" id="circle-me">
+                    	<br>
+                    	<p>My name is Luke Tanprasit.</p>
+                    	<p>I'm a student studying Computer Science BSc at Cardiff University.</p>
+                    	<p>Presently, I am a software intern at Alacrity Foundation. Working on web development and android projects.</p>
+                        <a href="#page-top" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- portfolio Section -->
+    <section id="portfolio" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 section-container">
+                <h2>PORFOLIO</h2>
+                <p>These projects are currently work in progress! Feel free to ask me any questions.</p>
+                <a href="http://www.deckcitadel.com/magic-the-gathering/221/zendikar/26945/adventuring-gear">
+                	<img class="thumbnail mini-thumbnail center-block" src="img/dc_preview.png">
+                </a>
+                <p>
+                	Social platform for card enthusiasts. Users can rate, build decks and search for latest card decriptions for over 20,000 cards.
+                </p>	
+            </div>
+        </div>
+    </section>
+
+    <!-- cv Section -->
+    <section id="resume" class="content-section text-center">
+        <div class="resume-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                	<div class="row">
+                		<h2>RESUME</h2>
+                		<p>
+                			Click here for my
+                			<a href="cv/version_01_02_2015.pdf">full cv</a>.
+                		</p>
+                	</div>
+                    <div class="row">
+                    	<div class="col-lg-6">
+                    		<H4>Skills</H4>
+                    		<ul class="list-unstyled">
+                    			<li>Java</li>
+                    			<li>PHP</li>
+                    			<li>JavaScript</li>
+                    			<li>Agile Methodologies</li>
+                    			<li>Python</li>
+                    			<li>SQL</li>
+                    			<li>Laravel</li>
+                    		</ul>
+                    	</div>
+                    	<div class="col-lg-6">
+                    		<H4>Interests</H4>
+                    		<ul class="list-unstyled">
+                    			<li>Web development</li>
+                    			<li>Comsumer Electronics</li>
+                    			<li>Custom Computers builds</li>
+                    			<li>Drawing</li>
+                    		</ul>
+                    	</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>CONTACT</h2>
+                <p>Feel free to contact me, if you have any queries.</p>
+                <p><a href="mailto:tanprasitp@hotmail.co.uk">tanprasitp@hotmail.co.uk</a>
+                </p>
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://twitter.com/tanprasitp" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Tanprasit" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/113357731441811735238" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; Tanprasit.co.uk</p>
+        </div>
+    </footer>
+
 </body>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
 </html>
